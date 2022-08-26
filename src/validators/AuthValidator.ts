57471da11877132a4f5c07e251c7b13e4a28fbb0
Gabriel Cortes Cassiano Pereira <ccpereiragabriel@gmail.com>
2022-08-26 16:43:50 -0300

    refactor: improving validation error messages

--- src/validators/AuthValidator.ts
+++ src/validators/AuthValidator.ts
@@ -3,8 +3,8 @@ import * as yup from 'yup';
 class AuthValidator {
   authValidation() {
     return yup.object().shape({
-      email: yup.string().email('E-mail incorreto').required('E-mail é obrigatório'),
-      password: yup.string().required('Senha é obrigatória'),
+      email: yup.string().email().required(),
+      password: yup.string().required(),
     });
   }
 }
